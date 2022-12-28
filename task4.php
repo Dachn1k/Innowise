c9832745286c4803f2a77c724416bac398ea3621
Dachn1k <7517097@gmail.com>
2022-12-28 20:59:09 +0300

    task4 reworked

--- task4.php
+++ task4.php
@@ -1,8 +1,7 @@
 <?php
-function foo(array $arr, int $position):array{
-    array_splice($arr, $position,1);
-    print_r($arr);
+function foo(array $arr, int $position):array
+{
+    array_splice($arr, $position, 1);
     return $arr;
 }
 foo($arr = [1,2,3,4,5], 4);
-?>
