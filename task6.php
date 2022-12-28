bec2a085ecd218bf52aeaaf8cda1a0b9cc8aac20
Dachn1k <7517097@gmail.com>
2022-12-28 21:04:44 +0300

    task6 reworked

--- task6.php
+++ task6.php
@@ -1,9 +1,9 @@
 <?php
+$string = "              The quick-brown_fox jumps over the_lazy-dog       ";
 function foo(string $string):string{
     $searchItems = [" ", "-", "_"];
     trim($string);
     $letterUpString = ucwords($string, " _-");
     return  str_replace( $searchItems, "", $letterUpString);
 }
-echo foo($string = "              The quick-brown_fox jumps over the_lazy-dog       ");
-?>
+echo foo($string);
