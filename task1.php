d316e111b4a763b2f7c152adf35bfca688e61128
Dachn1k <7517097@gmail.com>
2023-01-03 11:36:10 +0300

    reworked

--- task1.php
+++ task1.php
@@ -1,56 +1,63 @@
-<!doctype html>
-<html lang="">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-
 <?php
 
 // Write a PHP functions to test whether a number is greater than 30, 20 or 10
 // using a if conditions, switch and ternary operator.
-function foo1($number)
+function foo1(int $number):string
 {
-    if ($number > 30)
-        echo "More than 30 <br>";
-    if ($number > 20 && $number <= 30)
-        echo "More than 20 <br>";
-    if ($number > 10 && $number <= 20)
-        echo "More than 10 <br>";
-    if ($number <= 10)
-        echo "Equal or less than 10 <br>";
-}
-foo1(31);
-function foo2($number){
-    switch (true){
-        case $number > 30:
-            echo "More than 30 <br>";
-            break;
-        case $number > 20 && $number <= 30:
-            echo "More than 20 <br>";
-            break;
-        case $number > 10 && $number <= 20:
-            echo "More than 10 <br>";
-            break;
-        case $number <= 10:
-            echo "Equal or less than 10 <br>";
-            break;
+    $responseText = "";
+    if ($number > 30) {
+        $responseText = "More than 30";
+    }
+    if ($number > 20 && $number <= 30) {
+        $responseText = "More than 20";
+    }
+    if ($number > 10 && $number <= 20) {
+        $responseText = "More than 10";
+    }
+    if ($number <= 10) {
+        $responseText = "Equal or less than 10";
     }
 
+    return $responseText;
 }
-foo2(0);
 
-function foo3($number){
-    echo ($number > 30) ? "More than 30" : "";
-    echo ($number > 20) ? "More than 20" : "";
-    echo ($number > 10) ? "More than 10" : "";
-    echo ($number <= 10) ? "Equal or less than 10" : "";
+echo foo1(31);
+function foo2(int $number):string
+{
+    $responseText = "";
+    switch (true) {
+        case $number > 30:
+            $responseText = "More than 30";
+            break;
+        case $number > 20 && $number <= 30:
+            $responseText = "More than 20";
+            break;
+        case $number > 10 && $number <= 20:
+            $responseText = "More than 10";
+            break;
+        default:
+            $responseText = "Equal or less than 10";
+    }
+
+    return $responseText;
 }
-foo3(1);
+
+echo foo2(11);
+
+function foo3(int $number) :string
+{
+    return $number > 30
+        ? 'More than 30'
+        : ($number > 20
+            ? 'More than 20'
+            : ($number > 10
+                ? 'More than 10'
+                : ($number <= 10
+                    ? 'Equal or less than 10'
+                    : "")));
+
+
+}
+
+echo foo3(1);
 ?>
-</body>
-</html>
