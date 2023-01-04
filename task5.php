097dafb35e50f0b1c325e9b3a382346cde895e4b
Dachn1k <7517097@gmail.com>
2023-01-04 15:22:44 +0300

    task5

--- task5.php
+++ task5.php
@@ -1,15 +1,19 @@
 <?php
 
-function digitSequence(int $number1, int $number2):void
+function digitSequence(int $number1, int $number2): void
 {
     if ($number1 > $number2) {
         echo $number1-- . ',';
         digitSequence($number1, $number2);
-
-        
-    }elseif ($number1 < $number2) {
+        if ($number2 === $number1) {
+            echo $number1++;
+        }
+    } elseif ($number1 < $number2) {
         echo $number1++ . ',';
         digitSequence($number1, $number2);
-    } 
+        if ($number2 === $number1) {
+            echo $number1++;
+        }
+    }
 }
-digitSequence(1, 7);
+digitSequence(7, 1);
